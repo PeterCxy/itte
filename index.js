5b0d1f5ef6deeb85418084b7291fcd9f70ae8a47
Peter Cai <peter@typeblog.net>
2019-12-14 19:23:00 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,96 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+const REGEX_EMAIL = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
+async function handleRequest(request) {
+  let url = new URL(request.url)
+
+  for (key of Object.keys(HANDLERS)){
+    if (url.pathname.startsWith(key) && HANDLERS[key][request.method] != null) {
+      return HANDLERS[key][request.method](request, url)
+    }
+  }
+
+  return new Response(null, {
+    status: 404
+  })
+}
+
+async function postComment(request, url) {
+  var data
+
+  try {
+    data = await request.json()
+  } catch (err) {
+    return new Response("Invalid JSON object", {
+      status: 400
+    })
+  }
+
+  try {
+    validateCommentObject(data)
+  } catch (err) {
+    return new Response(err, {
+      status: 400
+    })
+  }
+
+  data.created_at = Date.now()
+  data.content = sanitizeHTML(data.content)
+
+  // Complement the actual key in store
+  // When lexicographically ordered, this will be in reversed-time order
+  // which is what we want
+  let dateKey = Number.MAX_SAFE_INTEGER - data.created_at
+
+  let key = `comment:${data.path}:${dateKey}`
+
+  await KV.put(key, JSON.stringify(data))
+
+  delete data.secret
+
+  return new Response(JSON.stringify(data), {
+    headers: {
+      "content-type": "application/json"
+    }
+  })
+}
+
+function validateCommentObject(obj) {
+  if (typeof obj.path != "string" || obj.path.length >= 255) {
+    throw "No Valid Path Provided"
+  }
+
+  if (typeof obj.secret != "string" || obj.secret.length >= 255) {
+    throw "No Valid Secret Provided"
+  }
+
+  if (typeof obj.content != "string" || obj.content.length >= 1024) {
+    throw "No Valid Content Provided"
+  }
+
+  if (typeof obj.username != "string" || obj.username.length >= 32) {
+    throw "No Valid User Name Provided"
+  }
+
+  if (typeof obj.email != "string" || obj.email.length >= 255) {
+    throw "No E-mail Provided"
+  }
+
+  if (!REGEX_EMAIL.test(obj.email)) {
+    throw "Malformed E-mail"
+  }
+}
+
+function sanitizeHTML(str) {
+  return str.replace("<", "&lt;")
+    .replace(">", "&gt;")
+}
+
+const HANDLERS = {
+  "/comments": {
+    "PUT": postComment
+  }
+}
